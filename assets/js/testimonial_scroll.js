1c8053e33c91a899466270f8dd8346c8042b2d10
Luis Zugasti <luis.zugasti@outlook.com>
2021-12-05 21:34:53 -0600

    Allow random start of testimonial

--- assets/js/testimonial_scroll.js
+++ assets/js/testimonial_scroll.js
@@ -1,7 +1,11 @@
+// constants
+const testimonialCount = 20;
+
 // setup
 const testimonials = document.getElementsByClassName("testimonial");
-let testimonialId = 0;
+let testimonialId = Math.floor(Math.random() * testimonialCount);
 const interval = window.setInterval(callback, 7500);
+testimonials[testimonialId].style.opacity = 1;
 
 // loop
 function callback() {
