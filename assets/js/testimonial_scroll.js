885c455d73d90177218922552ab9820156ed429c
Luis Zugasti <luis.zugasti@outlook.com>
2021-12-12 21:09:31 -0600

    Fix footers and fix some issues with scroll logic

--- assets/js/testimonial_scroll.js
+++ assets/js/testimonial_scroll.js
@@ -3,14 +3,16 @@ const testimonialCount = 20;
 
 // setup
 const testimonials = document.getElementsByClassName("testimonial");
-let testimonialId = Math.floor(Math.random() * testimonialCount);
-const interval = window.setInterval(callback, 7500);
-testimonials[testimonialId].style.opacity = 1;
-
-// loop
-function callback() {
-  testimonials[testimonialId].style.opacity = 0;
-  testimonialId = (testimonialId + 1) % testimonials.length;
+if (testimonials.length > 0) {
+  let testimonialId = Math.floor(Math.random() * testimonialCount);
+  const interval = window.setInterval(testimonialCallback, 7500);
   testimonials[testimonialId].style.opacity = 1;
-  console.log(testimonialId);
+
+  // loop
+  function testimonialCallback() {
+    testimonials[testimonialId].style.opacity = 0;
+    testimonialId = (testimonialId + 1) % testimonials.length;
+    testimonials[testimonialId].style.opacity = 1;
+  }
 }
+
