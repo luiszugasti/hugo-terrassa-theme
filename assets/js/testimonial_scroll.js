4eac8c850eb08c5d2682b5db463e9837466aee3a
Luis Zugasti <luis.zugasti@outlook.com>
2022-01-26 21:01:32 -0600

    Fix an issue where testimonials would bunch up

--- assets/js/testimonial_scroll.js
+++ assets/js/testimonial_scroll.js
@@ -1,18 +1,12 @@
-// constants
-const testimonialCount = 20;
-
 // setup
 const testimonials = document.getElementsByClassName("testimonial");
-if (testimonials.length > 0) {
-  let testimonialId = Math.floor(Math.random() * testimonialCount);
-  const interval = window.setInterval(testimonialCallback, 7500);
-  testimonials[testimonialId].style.opacity = 1;
+let currentTestimonial = Math.floor(Math.random() * testimonials.length);
+testimonials[currentTestimonial].style.opacity = 1;
+const callBackFunction = window.setInterval(testimonialCallback, 6500);
 
-  // loop
-  function testimonialCallback() {
-    testimonials[testimonialId].style.opacity = 0;
-    testimonialId = (testimonialId + 1) % testimonials.length;
-    testimonials[testimonialId].style.opacity = 1;
-  }
+// loop
+function testimonialCallback() {
+  testimonials[currentTestimonial].style.opacity = 0;
+  currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+  testimonials[currentTestimonial].style.opacity = 1;
 }
-
