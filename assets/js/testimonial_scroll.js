59ae0cc16c8910887118562bb9fb7c15b2f2c977
Luis <luis.zugasti@outlook.com>
2022-04-03 21:15:59 -0500

    Final nail on testimonials. We also have a lot more entries in the nav, so making the hamburger menu show up a lot earlier

--- assets/js/testimonial_scroll.js
+++ assets/js/testimonial_scroll.js
@@ -2,11 +2,14 @@
 const testimonials = document.getElementsByClassName("testimonial");
 let currentTestimonial = Math.floor(Math.random() * testimonials.length);
 testimonials[currentTestimonial].style.opacity = 1;
+testimonials[currentTestimonial].style.zIndex = 90;
 const callBackFunction = window.setInterval(testimonialCallback, 6500);
 
 // loop
 function testimonialCallback() {
   testimonials[currentTestimonial].style.opacity = 0;
+  testimonials[currentTestimonial].style.zIndex = 0;
   currentTestimonial = (currentTestimonial + 1) % testimonials.length;
   testimonials[currentTestimonial].style.opacity = 1;
+  testimonials[currentTestimonial].style.zIndex = 90;
 }
