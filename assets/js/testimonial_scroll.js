30c06e74cad4bd8dda746f4a58f8ead66d00ca19
Luis <luis.zugasti@outlook.com>
2022-04-19 17:17:13 -0500

    Comment out debug code, quit if not on testimonial

--- assets/js/testimonial_scroll.js
+++ assets/js/testimonial_scroll.js
@@ -1,15 +1,22 @@
-// setup
-const testimonials = document.getElementsByClassName("testimonial");
-let currentTestimonial = Math.floor(Math.random() * testimonials.length);
-testimonials[currentTestimonial].style.opacity = 1;
-testimonials[currentTestimonial].style.zIndex = 90;
-const callBackFunction = window.setInterval(testimonialCallback, 6500);
+function testimonialLoop() {
+  // setup
+  const testimonials = document.getElementsByClassName("testimonial");
 
-// loop
-function testimonialCallback() {
-  testimonials[currentTestimonial].style.opacity = 0;
-  testimonials[currentTestimonial].style.zIndex = 0;
-  currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+  if (testimonials.length ===0) { return; }
+
+  let currentTestimonial = Math.floor(Math.random() * testimonials.length);
   testimonials[currentTestimonial].style.opacity = 1;
   testimonials[currentTestimonial].style.zIndex = 90;
+  const callBackFunction = window.setInterval(testimonialCallback, 6500);
+
+  // loop
+  function testimonialCallback() {
+    testimonials[currentTestimonial].style.opacity = 0;
+    testimonials[currentTestimonial].style.zIndex = 0;
+    currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+    testimonials[currentTestimonial].style.opacity = 1;
+    testimonials[currentTestimonial].style.zIndex = 90;
+  }
 }
+
+testimonialLoop();
